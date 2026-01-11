d38e9c259d9ce2e7062b5df77f74c9b9a1b4e500
tekane404 <benedictegarry@gmail.com>
2026-01-11 17:28:38 +0100

    hgfds

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import './style.css'
-import * as THREE from 'three';
+import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 // Scène
