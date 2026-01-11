528cca3b240870e7a3e4dd9f6886948bb105944c
tekane404 <benedictegarry@gmail.com>
2026-01-11 17:49:37 +0100

    3M5GJOP

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-import './style.css'
 import * as THREE from "https://unpkg.com/three@0.160.0/build/three.module.js";
 import { OrbitControls } from "https://unpkg.com/three@0.160.0/examples/jsm/controls/OrbitControls.js";
 
