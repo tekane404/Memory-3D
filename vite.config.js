1d0429bb6dd37327578d4fa062b0d3f43bbfbdb0
tekane404 <benedictegarry@gmail.com>
2026-01-11 17:39:56 +0100

    oiijihih

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
+import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'path'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   resolve: {
     alias: {
-      'three/examples/jsm': 'three/examples/jsm'
+      'three/examples/jsm': resolve(__dirname, 'node_modules/three/examples/jsm')
     }
-  },
-  optimizeDeps: {
-    include: ['three', 'three/examples/jsm/controls/OrbitControls']
   }
 })
